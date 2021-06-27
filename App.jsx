735c2376578364b3297a300ce14522eb0a5c5cdc
sangeeta-11 <85803103+sangeeta-11@users.noreply.github.com>
2021-06-27 16:50:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "./Footer";
+import Info from "./Info";
+import Header from "./Head";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript anf React.js</h1>
+      <p> A basic Web Development React js Bootcamp By Shaurya Sinha Sir</p>
+    </div>
+  );
+}
+export default Info;
